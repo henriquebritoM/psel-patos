7a25f16ba70a1b95e364337562f5e46db6b60f60
Henrique <henrique.britomelo667@gmail.com>
2025-10-03 12:11:27 -0300

    async implementado para tcp_wrapper

--- libs/tcp_wrapper/tests/send.rs
+++ libs/tcp_wrapper/tests/send.rs
@@ -2,15 +2,15 @@
 #[cfg(test)] 
 mod testes {
 
-    use std::net::{TcpListener, TcpStream};
+    use tokio::net::{TcpListener, TcpStream};
 
     use http_parser::*;
     use tcp_wrapper::*;
 
-    #[test]
-    fn test_to_default_to_string() {
-        let listener = TcpListener::bind("localhost:9090").unwrap();
-        let mut sender = TcpStream::connect("localhost:9090").unwrap();
+    #[tokio::test]
+    async fn test_to_default_to_string() {
+        let listener = TcpListener::bind("localhost:9090").await.unwrap();
+        let mut sender = TcpStream::connect("localhost:9090").await.unwrap();
 
         let req_enviada = Request::new()
                             .method(http_parser::Method::DELETE)
@@ -19,11 +19,11 @@ mod testes {
                             .add_header("Content-Length", "3")
                             .body("123");   
 
-        let (mut receiver, _): (TcpStream, std::net::SocketAddr) = listener.accept().unwrap();
+        let (mut receiver, _): (TcpStream, std::net::SocketAddr) = listener.accept().await.unwrap();
 
-        write_stream(&mut sender, &req_enviada.as_bytes()).unwrap();
+        write_stream(&mut sender, &req_enviada.as_bytes()).await.unwrap();
 
-        let req_recebida = read_request(&mut receiver);
+        let req_recebida = read_request(&mut receiver).await;
 
         assert_eq!(Some(Ok(req_enviada)), req_recebida);
     }
